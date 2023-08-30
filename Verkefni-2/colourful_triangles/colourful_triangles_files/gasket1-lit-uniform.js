c4348b1dc11f387eec85a2b3b52cf4b9159ba67f
Andri Fannar Kristjánsson <Andri@Hilti.is>
2023-08-30 21:03:03 +0000

    Uniform triangles

--- Verkefni-2/colourful_triangles/colourful_triangles_files/gasket1-lit-uniform.js
+++ Verkefni-2/colourful_triangles/colourful_triangles_files/gasket1-lit-uniform.js
@@ -3,6 +3,7 @@ var points;
 
 // Hversu marga þríhyrninga á að teikna.
 var NumTriangles = 100;
+var posLoc;
 var colorLoc;
 
 window.onload = function init()
@@ -12,24 +13,8 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    // Búa til fylkið sem á að halda utan um punktana.
-    let totalSize = NumTriangles * 6;
-    let vertices = new Float32Array(totalSize);
-
-    // Finna staðsetningu af handahófi fyrir hvern þríhyrning sem á að teikna.
-    for(i = 0; i < totalSize; i = i + 6)
-    {
-        // Finna stað af handahófi fyrir efsta punkt þríhyrningsins.
-        vertices[i]     = ((Math.random() * 2) - 1);
-        vertices[i + 1] = ((Math.random() * 2) - 1);
-        
-        // Út frá efsta punktinum, reikna staðsetningu neðri punktana svo allir þríhyrningarnir hafi sömu stærð.
-        vertices[i + 2] = (vertices[i] - 0.08);
-        vertices[i + 3] = (vertices[i + 1] - 0.15);
-
-        vertices[i + 4] = (vertices[i] + 0.08);
-        vertices[i + 5] = (vertices[i + 1] - 0.15);
-    }
+    // Búa til fylki sem inniheldur einn þríhyrning í miðju.
+    var vertices = new Float32Array([ 0.0, 0.0, -0.16, -0.3, 0.16, -0.3 ]);
 
     //
     //  Configure WebGL
@@ -38,24 +23,23 @@ window.onload = function init()
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
     
     //  Load shaders and initialize attribute buffers
-    
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
     // Load the data into the GPU
-    
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, vertices/*flatten(points)*/, gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
 
     // Associate shader variables with our data buffer
-    
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
     // Find the location of the variable fColor in the shader program
     colorLoc = gl.getUniformLocation( program, "fColor" );
+    // Finnum staðsetningu translation breytunnar í hnútalitaranum
+    posLoc = gl.getUniformLocation( program, "translation");
     
     render();
 };
@@ -65,6 +49,7 @@ function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
 
     let colours = [0.0, 0.0, 0.0];
+    let movement;
 
     // Keyra fyrir hvern þríhyrning sem á að teikna.
     for(i = 0; i < NumTriangles; i++)
@@ -75,8 +60,12 @@ function render() {
             colours[j] = Math.random();
         }
 
-        // Setja litinn og teikna þríhyrninginn.
+        // Búum til nýja staðsetningu af handahófi til að færa þríhyrninginn á.
+        movement = [((Math.random() * 4) - 2), ((Math.random() * 4) - 2)];
+
+        // Setja litinn, senda breytta staðsetningu á hnútalitarann og teikna þríhyrninginn.
         gl.uniform4fv( colorLoc, vec4(colours[0], colours[1], colours[2], 1.0) );
-        gl.drawArrays( gl.TRIANGLES, (3 * i), 3 );
+        gl.uniform4fv( posLoc,   vec4(movement, 0.0, 0.0) );
+        gl.drawArrays( gl.TRIANGLES, 0, 3 );
     }
-}
+} 
