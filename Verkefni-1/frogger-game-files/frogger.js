ab1042c3e342b50dd99ca7959157427f4d7b1839
Andri Fannar Kristjánsson <Andri@Hilti.is>
2023-09-06 23:03:09 +0000

    Cars Movement

--- Verkefni-1/frogger-game-files/frogger.js
+++ Verkefni-1/frogger-game-files/frogger.js
@@ -3,21 +3,43 @@
 var canvas;
 var gl;
 
+// Heildar stigafjöldi leikmanns.
 var points = 0;
 
 // Staðsetning hnúta sem á að teikna.
 var gameBoard = [];
 
-// Uniform breytur sem tengjast liturum.
+// Stærð akgreinanna.
+var laneSize = 0.40;
+
+// Uniform breyta sem stjórnar lit á hlutunum.
 var colorLoc;
+
+// Uniform breyta sem stjórnar staðsetningu hluta.
 var positionLoc;
+
+// Uniform breyta sem stjórnar snúningi hluta.
 var sinCosLoc;
+
+// Sínus og Kósínus gildin sem á að senda á hnútalitarann (snúningur hlutarins).
 var sinCos  = vec2( 0.0, -1.0);
 
+// Núverandi staðsetning leikmanns (skipt í akgreinar).
 var lane = [0.0 , -2.0];
+
+// Er froskurinn á uppleið.
 var goingUp = true;
 
-var laneSize = 0.40;
+// Staðsetning frosksins í hnitum.
+var frogPos = [ 0.0 , 0.0 ];
+
+// Hraði bílanna.
+var carSpeed = 0.01;
+
+// Staðsetning bílanna.
+var carsX = [ 0.0 , 0.0 , 0.0 ];
+
+// Litir notaðir í forritinu.
 var gameColours = {
     gray:   vec4(0.671, 0.671, 0.671, 1.0),
     yellow: vec4(0.929, 0.812, 0.078, 1.0),
@@ -25,13 +47,19 @@ var gameColours = {
     blue:   vec4(0.071, 0.408, 0.749, 1.0)
 };
 
+
+/**
+ * Fall sem upphafsstillir WebGL og þar með leikinn.
+ */
 window.onload = function init()
 {
+    // Hlaða inn canvas hlutanum.
     canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
+    // Upphafsstaðsetningar leikhluta á leikborði fyrir breytingar.
     gameBoard = [
         // Gangstétt
         vec2( -1.0 , -0.6 ),
@@ -55,12 +83,12 @@ window.onload = function init()
         vec2(  0.1 , -0.1 ),
 
         // Bíll
-        vec2( -0.9 , -0.3 ),
-        vec2( -0.9 , -0.5 ),
-        vec2( -0.6 , -0.3 ),
-        vec2( -0.9 , -0.5 ),
-        vec2( -0.6 , -0.3 ),
-        vec2( -0.6 , -0.5 ),
+        vec2( -1.4 , -0.3 ),
+        vec2( -1.4 , -0.5 ),
+        vec2( -1.1 , -0.3 ),
+        vec2( -1.4 , -0.5 ),
+        vec2( -1.1 , -0.3 ),
+        vec2( -1.1 , -0.5 )
     ];
 
 
@@ -87,69 +115,87 @@ window.onload = function init()
     positionLoc = gl.getUniformLocation( program, "translation");
     sinCosLoc = gl.getUniformLocation( program, "sinCos");
 
+    // Virkja hreyfingu leikmanns.
     movement();
     
+    // Teikna á skjáinn.
     render();
 };
 
+/**
+ * Fall sem sér um hreyfingu leikmanns.
+ */
 function movement()
 {
     window.addEventListener("keydown", function(e) 
     {
         switch(e.key) {
             case "ArrowUp":
-                if (lane[1] === 2) break;
-                lane[1]++;
-                if (lane[1] === 2) turnAround();
+                // Athuga hvort leikmaður sé kominn út á enda.
+                if (lane[1] === 2) break; // Ef svo er þá á hann ekki að færast lengra.
+                lane[1]++; // Færa leikmann áfram um eina akgrein.
+                if (lane[1] === 2) turnAround(); // Ef leikmaður er kominn út á enda (aðeins á y-ás), þá á að snúa honum við.
                 break;
 
             case "ArrowDown":
                 if (lane[1] === -2) break;
                 lane[1]--;
-                if (lane[1] === -2) turnAround();
                 break;
 
             case "ArrowLeft":
-                if (lane[0] === -2) break;
-                lane[0]--;
-                break;
-
-            case "ArrowRight":
                 if (lane[0] === 2) break;
                 lane[0]++;
                 break;
+
+            case "ArrowRight":
+                if (lane[0] === -2) break;
+                lane[0]--;
+                break;
         }
+
+        // Reikna staðsetningu frosksins.
+        frogPos[0] = laneSize * lane[0];
+        frogPos[1] = laneSize * lane[1];
     })
 }
 
+/**
+ * Fall sem sér um að snúa leikmanni við.
+ */
 function turnAround()
 {
-    if ( lane[1] === -2 ) return;
-    else 
-    {
-        points++;
+    // Auka stigafjölda.
+    points++;
 
-        if (goingUp)
-        {
-            sinCos[0] = Math.sin(0);
-            sinCos[1] = Math.cos(0);
-        }
-        else 
-        {
-            sinCos[0] = Math.sin(Math.PI);
-            sinCos[1] = Math.cos(Math.PI);
-        }
-        lane[0] = -lane[0];
-        lane[1] = -lane[1];
-        goingUp = !goingUp;
+    // Ef leikmaðurinn er á leið upp á að snúa honum niður.
+    if (goingUp)
+    {
+        sinCos[0] = Math.sin(0);
+        sinCos[1] = Math.cos(0);
     }
+    else 
+    {   // Annars á að snúa honum upp.
+        sinCos[0] = Math.sin(Math.PI);
+        sinCos[1] = Math.cos(Math.PI);
+    }
+    // Snúa akgreinum y-ás við (akgrein -2 á alltaf að vera aftast).
+    lane[1] = -lane[1];
+    goingUp = !goingUp;
+
+}
+
+function collisionDetection()
+{
+    console.log(gameBoard[13] + frogPos);
 }
 
 function render()
 {
+    // Stilla hnúta og bútalitarann fyrir fyrstu teiknun.
     gl.uniform2fv( sinCosLoc, vec2( 0.0 , -1.0));
     gl.uniform4fv( colorLoc, gameColours.gray );
 
+    // Hreinsa teikniborðið
     gl.clear( gl.COLOR_BUFFER_BIT );
 
     // Teikna gangstéttina.
@@ -159,10 +205,16 @@ function render()
         gl.drawArrays( gl.TRIANGLES, 0, 6 );
     }
 
-    gl.uniform4fv( colorLoc, gameColours.blue );
-    gl.uniform2fv( positionLoc, vec2(0 , 0));
-    gl.drawArrays( gl.TRIANGLES, 15, 6 );
-
+    // Teikna bíla.
+    for(let i = 0; i < 3; i++)
+    {
+        gl.uniform4fv( colorLoc, gameColours.blue );
+        carsX[i] = carsX[i] + carSpeed * (i + 1);
+        if (carsX[i] > 2.5) carsX[i] = 0;
+        gl.uniform2fv( positionLoc, vec2( carsX[i] , laneSize * i));
+        gl.drawArrays( gl.TRIANGLES, 15, 6 );
+    }
+    
     gl.uniform2fv( positionLoc, vec2(0.0 , 0.0));
     gl.uniform4fv( colorLoc, gameColours.yellow );
 
@@ -175,7 +227,7 @@ function render()
     gl.uniform2fv( positionLoc, vec2(0.0 , 0.0));
 
     gl.uniform4fv( colorLoc, gameColours.green );
-    gl.uniform2fv( positionLoc, vec2(laneSize * lane[0], laneSize * lane[1]));
+    gl.uniform2fv( positionLoc, frogPos);
     gl.uniform2fv( sinCosLoc, sinCos);
     gl.drawArrays( gl.TRIANGLES, 12, 3 );
 
